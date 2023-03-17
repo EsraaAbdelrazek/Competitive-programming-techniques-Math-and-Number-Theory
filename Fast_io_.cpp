10175fe9bf48dc9b2e8f6053145bb7747410f35f
Esraa Ahmed <99386832+EsraaAbdelrazek@users.noreply.github.com>
2023-03-17 17:01:07 +0200

    Add files via upload

---
+++ Fast_io_.cpp
@@ -0,0 +1,45 @@
+using namespace std;
+#include<bits/stdc++.h>
+#define fast        ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define ll long long
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define pb   push_back
+#define clr(x ,val) memset(x,val , sizeof(x))
+  int main ()
+  {
+  //	freopen("input.in" , "r" , stdin) ; 
+  
+  	int x ;
+  	scanf ("%d" , &x );
+  	printf ("%d\n" , x );
+  	
+  	int  u , v  ;
+  	scanf ("%d %d" , &u , &v );
+  	printf ("%d %d\n" , u , v );
+  	
+  	long long  y  ;
+  	scanf ("%lld" , &y );
+  	printf ("%lld\n" , y );
+  	
+  	float z  ;
+  	scanf ("%f" , &z );
+  	printf ("%f\n" , z );
+  	float c  ;
+  	scanf ("%f" , &z );
+  	printf ("%0.2f\n" , z );
+	
+	double a  ;
+  	scanf ("%lf" , &a );
+  	printf ("%0.4lf\n" , a );
+  	
+  	char b ; 
+  	scanf (" %c" , &b) ; 
+  	printf("%c\n" , b) ; 
+
+	char s[5] ; 
+	scanf(" %s" , s)  ; // s is pointer
+	printf("%s\n" , s) ; 
+  //	printf(" %c" , "==================================\n") ; 
+  
+  	return 0 ; 
+  }
